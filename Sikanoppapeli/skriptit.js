155743e20048573ef565dae297f03a4fa1bb3751
Juuso Saltiola <juuso.saltiola@itvelhot.local>
2019-10-04 12:58:10 +0300

    muutoksiii

--- Sikanoppapeli/skriptit.js
+++ Sikanoppapeli/skriptit.js
@@ -1,5 +1,9 @@
-var pisteet = 0;
+var c=document.getElementById("nopanpaikka");
 var pelaaja1 = 0;
+var pisteet=0;
+var pistetaulukko = [0,0];
+var vuoro = 0;
+var kokopisteet = 0;
 var images = [
 "img/1.png",
 "img/2.png",
@@ -12,7 +16,20 @@ var images = [
 function pelaa() {
     let dice = Math.floor(Math.random() * 6) + 1;
     document.getElementById("noppa").innerHTML = dice;
-    pisteet += 4;
+    pisteet += dice;
     document.getElementById("pisteet").innerHTML = pisteet;
-    // document.getElementById("noppa").src =
+    document.getElementById("noppa").src = "img/"+images[dice - 1];
     }
+
+    function vaihdavuoro() {
+        console.log('vaihdavuoro');
+        if (vuoro === 0) {
+          pistetaulukko[0] += pisteet;
+          vuoro = 1;
+        } else {
+          pistetaulukko[1] += pisteet;
+          vuoro = 0;
+        }
+        pisteet = 0;
+        refresh();
+      }
